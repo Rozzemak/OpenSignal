221f234c3b37b48fb8aa500856a45a98cad0f86d
Vladimír Beneš <benes.vladimir98@gmail.com>
2019-05-04 07:00:46 +0200

    Server problem

--- graph.py
+++ graph.py
@@ -1,49 +1,64 @@
 from bokeh.models.callbacks import CustomJS
 from bokeh.layouts import grid, column, row
 from bokeh.models.widgets import Select, Button
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, curdoc
 from bokeh.events import ButtonClick
-
-output_file('dashboard.html')
-
-def callback(event):
-    print('Python:Click')
-
-def controls():
-    options = ["item 1", "item 2", "item 3"]
-    firstSignal = Select(title="First signal:", options=options)
-    secondSignal = Select(title="Second signal:", options=options)
-    findButton = Button(label="Find similarities", button_type="success")
-    findButton.on_event(ButtonClick, callback)
-    widgets = column(firstSignal, secondSignal, findButton)
-
-    return widgets
+import fetch_data as fd
+import pandas as pd
+from Parser import Parser as Pr
 
 
-def plotter():
-    x = [-3, 2, -1, 1]
-    y =  [-1, 0, -3, 2]
-
-    x1 = [-5, 3, -2, 1]
-    y1 = [-4, 4, -2, 1]
-
-    plot1 = figure(
-        y_range=(-5, 5), title="First signal", width=1250, height=350
-    )
-    plot1.line(x, y)
-
-    callback = CustomJS(code="""
-    console.log('Tap event occurred at x-position: ' + cb_obj.x)
-    """)
-    plot1.js_on_event('doubletap', callback)
-
-    plot2 = figure(
-        y_range=(-5, 5), title="Second signal", width=1250, height=350
-    )
-    plot2.line(x1, y1)
-    plots = column(plot1, plot2)
-    return plots
+class c_graph:
+    def __init__(self):
+        self.fetch_data = fd.FetchData("https://physionet.org/physiobank/database/emgdb/RECORDS")
+        self.l = self.fetch_data.get_data()
+        self.dfs = Pr.parse_to_csv(self.l)
+        output_file('dashboard.html')
+        self.load_csv()
 
 
-l = row(controls(), plotter())
-show(l)
+    def load_csv(self):
+        self.healthy = pd.read_csv("Files/emg_healthy.txt.csv")
+        self.myopathy = pd.read_csv("Files/emg_myopathy.txt.csv")
+        self.neuropathy = pd.read_csv("Files/emg_neuropathy.txt.csv")
+
+    def controls(self):
+        options = ["Healthy", "Myopathy", "Neurophaty"]
+        firstSignal = Select(title="First signal:", options=options)
+        secondSignal = Select(title="Second signal:", options=options)
+        findButton = Button(label="Find similarities", button_type="success")
+        #findButton.on_event(ButtonClick, self.callback)
+        #firstSignal.on_change("value", print("value"))
+
+        widgets = column(firstSignal, secondSignal, findButton)
+        return widgets
+
+    def plotter(self):
+        x1 = self.healthy["x"]
+        y1 = self.healthy["y"]
+        x = [-3, 2, -1, 1]
+        y = [-1, 0, -3, 2]
+
+        x2 = [-5, 3, -2, 1]
+        y2 = [-4, 4, -2, 1]
+
+        plot1 = figure(
+            tools="pan,zoom_in,zoom_out",
+            title="First signal", width=1200, height=350,
+            y_range=(-0.5, 0.5),
+            x_range=(1, 1.5)
+        )
+        plot1.line(x1, y1)
+
+        plot2 = figure(
+            title="Second signal", width=1200, height=350,
+            y_range=(-0.5, 0.5),
+            x_range=(1, 1.5)
+        )
+        plot2.line(x2, y2)
+        plots = column(plot1, plot2)
+        return plots
+
+    def run(self):
+        curdoc().add_root(row(self.controls(), self.plotter()))
+
--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-from Parser import Parser as Pr
-from fetch_data import FetchData
 from bokeh.plotting import figure, output_file, show
-import pandas as pd
-from bokeh.models import DatetimeTickFormatter, ColumnDataSource
-
-fetch_data = FetchData("https://physionet.org/physiobank/database/emgdb/RECORDS")
-l = fetch_data.get_data()
-dfs = Pr.parse_to_csv(l)
-
-healthy = pd.read_csv("Files/emg_healthy.txt.csv")
-myopathy = pd.read_csv("Files/emg_myopathy.txt.csv")
-neuropathy = pd.read_csv("Files/emg_neuropathy.txt.csv")
-
-#source = ColumnDataSource(dfs[0])
-
-#Take data  and present in a graph
-output_file("test.html")
-p = figure(plot_width=1280, plot_height=720)
-p.line(x='x',y='y',line_width=1, source=neuropathy)
-
-show(p)
+import graph
+g = graph.c_graph()
+g.run()
