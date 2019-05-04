bf191789151dabc44714832531557377f874ad3f
Marek Slavka <rozzemak@gmail.com>
2019-05-04 13:02:32 +0200

    Corelation fixes, #19

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -19,7 +19,10 @@ def parse_to_csv_local(file):
     if os.path.isfile("Files/" + file+'.csv'):
         os.remove("Files/" + file+'.csv')
     df = pd.read_csv(file, delim_whitespace=True, header=None, encoding="windows-1250")
+    df1 = pd.to_numeric(df[1], errors='coerce')
+    df[1] = df1
     df.to_csv(file+'.csv', index=False, header=["x","y"])
+    print(df.dtypes)
     if not os.path.isdir("Files"):
         os.mkdir("Files")
     shutil.move(file+'.csv', "Files/" + file+'.csv')
